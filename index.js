9931f824c245164a78be62cd875a38ff307a7dba
De Coster <nicolasdecoster221@hotmail.com>
2021-02-09 15:55:33 +0100

    exo2 : todo POST

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000
 app.use(express.static('public'))
 app.use(express.urlencoded())
 
-app.get('/', (req, res) => res.redirect('/todo'))
+app.get('/', (req, res) => res.render('home.ejs'))
 
 const todoRoutes = require('./routes/todo')
 app.use('/todo', todoRoutes)
